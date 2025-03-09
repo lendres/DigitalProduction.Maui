e1c425603ff8f86b3f931fc877d4d59fa67db321
Ikosy <lendres@fifthrace.com>
2025-03-09 18:43:13 -0500

    Add save file picker demo

---
+++ src/Demo/ViewModels/SaveFilePickerPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using DigitalProduction.Maui.Storage;
+
+namespace DigitalProduction.Demo.ViewModels;
+
+public partial class SaveFilePickerPageViewModel : BaseViewModel
+{
+	#region Fields
+
+	private ISaveFilePicker _saveFilePicker = DigitalProduction.Maui.Services.ServiceProvider.GetService<ISaveFilePicker>();
+
+	#endregion
+
+	#region Construction
+
+	public SaveFilePickerPageViewModel()
+	{
+	}
+
+	#endregion
+
+	#region Properties
+
+	// Input file.
+	[ObservableProperty]
+	public partial string		SaveFile { get; set; }							= string.Empty;
+
+	public ISaveFilePicker		SaveFilePicker { get => _saveFilePicker; }
+
+	#endregion
+
+}
