342448023e67612ff40cbf76077c18b6fc53725b
Ikosy <lendres@fifthrace.com>
2025-06-25 20:31:00 -0500

    Add enums

---
+++ src/Maui Library/Controls/MessageBoxYesNoToAllButtons.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace DigitalProduction.Maui.Controls;
+
+/// <summary>
+/// Buttons shown on a MessageBoxYesNoToAll dialog box.
+/// 
+/// The "Description" attribute can be accessed using Reflection to get a string representing the enumeration type.
+/// </summary>
+public enum MessageBoxYesNoToAllButtons
+{
+	/// <summary>Yes, Yes to All, No.</summary>
+	YesToAllNo,
+
+	/// <summary>Yes to All, No, Cancel.</summary>
+	YesToAllNoCancel,
+
+	/// <summary>Yes, No, No to All.</summary>
+	YesNoToAll,
+
+	/// <summary>Yes, No, No to All, Cancel.</summary>
+	YesNoToAllCancel,
+
+	/// <summary>Yes, Yes to All, No, No to All.</summary>
+	YesToAllNoToAll,
+
+	/// <summary>Yes, Yes to All, No, No to All, Cancel.</summary>
+	YesToAllNoToAllCancel
+
+} // End enum.
---
+++ src/Maui Library/Controls/MessageBoxYesNoToAllDefaultButton.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace DigitalProduction.Maui.Controls;
+
+/// <summary>
+/// Button that is the default on the form (button activated if "enter" is pressed).
+/// 
+/// The "Description" attribute can be accessed using Reflection to get a string representing the enumeration type.
+/// </summary>
+public enum MessageBoxYesNoToAllDefaultButton
+{
+	/// <summary>Button 1.</summary>
+	[Description("Button 1")]
+	Button1,
+
+	/// <summary>Button 2.</summary>
+	[Description("Button 2")]
+	Button2,
+
+	/// <summary>Button 3.</summary>
+	[Description("Button 3")]
+	Button3,
+
+	/// <summary>Button 4.</summary>
+	[Description("Button 4")]
+	Button4,
+
+	/// <summary>Button 5.</summary>
+	[Description("Button 5")]
+	Button5,
+
+} // End enum.
