dae467c74c17f61eaa9a133421b75a0e4b9fe057
lendres <lendres@fifthrace.com>
2026-04-23 16:47:22 -0500

    Add preferences

---
+++ src/Unit Tests/PreferencesTest.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using Preferences =  DigitalProduction.Maui.Storage.Preferences;
+
+namespace DigitalProduction.UnitTests;
+
+public class PreferencesTests
+{
+	public class TestSettings
+	{
+		public string Name { get; set; } = "";
+		public int Age { get; set; }
+	}
+
+	[Fact]
+	public void GetStringReturnsDefaultWhenKeyDoesNotExist()
+	{
+		Preferences.Remove();
+		string value = Preferences.Get("");
+		Assert.Equal("", value);
+
+		Preferences.Set("stringtest");
+		value = Preferences.Get("");
+
+		Assert.Equal("", value);
+	}
+
+	[Fact]
+	public void GetAndSetOfString()
+	{
+		Preferences.Remove();
+		string value = Preferences.Get("");
+		Assert.Equal("", value);
+
+		Preferences.Set("stringtest");
+		value = Preferences.Get("");
+
+		Assert.Equal("", value);
+	}
+
+
+}
